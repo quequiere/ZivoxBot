659ddfe96d913639b1b8bd80a5e218e3f2389f1c
mrzivox <35635645+mrzivox@users.noreply.github.com>
2018-01-20 18:42:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+// Partie Const ////////////////////////////////////////////////////////////
+
+
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const token = "MzkxMzcxMTk0MDMwNDg5NjEy.DT046Q.1YHe0HX_A4oFid32jNQ6voIvc_0";
+const prefix = "/";
+
+
+
+// Var ////////////////////////////////////////////////////////////
+
+
+var mention = "la mention du bot"
+var randnum =  0;
+
